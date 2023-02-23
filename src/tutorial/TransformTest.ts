f121005ebd54f036e6bf1bde77dcfb3b9ada21ae
vilyLei <vily313@126.com>
2023-02-23 23:14:35 +0800

    fix: add a animation function in the demo.

--- src/tutorial/TransformTest.ts
+++ src/tutorial/TransformTest.ts
@@ -1,7 +1,8 @@
 import IRendererScene from "../engine/vox/scene/IRendererScene";
-import { MouseEvent, VoxRScene } from "../engine/cospace/voxengine/VoxRScene";
+import { EventBase, MouseEvent, VoxRScene } from "../engine/cospace/voxengine/VoxRScene";
 import { VoxUIInteraction } from "../engine/cospace/voxengine/ui/VoxUIInteraction";
 import { VoxEntity } from "../engine/cospace/voxentity/VoxEntity";
+import { VoxMath } from "../engine/cospace/math/VoxMath";
 import { VoxMaterial } from "../engine/cospace/voxmaterial/VoxMaterial";
 import IRenderTexture from "../engine/vox/render/texture/IRenderTexture";
 import VoxModuleShell from "../common/VoxModuleShell";
@@ -9,52 +10,108 @@ import IRenderMaterial from "../engine/vox/render/IRenderMaterial";
 import IMouseEvent from "../engine/vox/event/IMouseEvent";
 import IMouseEventEntity from "../engine/vox/entity/IMouseEventEntity";
 
-const vertShaderCode = `#version 300 es
-precision mediump float;
-layout(location = 0) in vec3 a_vs;
-layout(location = 1) in vec2 a_uvs;
-uniform mat4 u_objMat;
-uniform mat4 u_viewMat;
-uniform mat4 u_projMat;
-out vec2 v_uv;
-void main()
-{
-    gl_Position = u_projMat * u_viewMat * u_objMat * vec4(a_vs,1.0);
-    v_uv = a_uvs;
-}
-`;
-const fragShaderCode = `#version 300 es
-precision mediump float;
-uniform sampler2D u_sampler0;
-uniform vec4 u_color;
-in vec2 v_uv;
-layout(location = 0) out vec4 FragColor0;
-void main()
-{
-    FragColor0 = texture(u_sampler0, v_uv.xy) * u_color;
-}
-`;
+import { PBREnvLightingMaterialWrapper } from "./material/PBREnvLightingMaterialWrapper";
+import ITransformEntity from "../engine/vox/entity/ITransformEntity";
+import IVector3D from "../engine/vox/math/IVector3D";
+import IEventBase from "../engine/vox/event/IEventBase";
+import IColor4 from "../engine/vox/material/IColor4";
 
-class MaterialObj {
+class AnimationScene {
+
+    private m_rscene: IRendererScene;
+    private m_envTex: IRenderTexture;
+    private m_sphEntity: ITransformEntity = null;
+    private m_entities: ITransformEntity[] = [];
     
-    material: IRenderMaterial;
-    colorData: Float32Array;
-    constructor(m: IRenderMaterial, d: Float32Array) {
-        this.material = m;
-        this.colorData = d;
+    constructor(sc: IRendererScene, envTex: IRenderTexture) {
+        this.m_rscene = sc;
+        this.m_envTex = envTex;
     }
-    setRGB3f(r: number, g: number, b: number): void {
-        const ls = this.colorData;
-        ls[0] = r;
-        ls[1] = g;
-        ls[2] = b;
+    initialize(): void {
+
+        let begin = VoxMath.createVec3(-300, 0, 300);
+        let offsetV = VoxMath.createVec3(30, 0, -30);
+        for (let i = 0; i < 20; ++i) {
+            this.createSphere(15, VoxMath.createVec3().copyFrom(offsetV).scaleBy(i).addBy(begin));
+        }
+        this.m_rscene.addEventListener(EventBase.ENTER_FRAME, this, this.run);
+    }
+    private createSphere(radius: number, pv: IVector3D): ITransformEntity {
+
+        let material = this.makeMaterial(Math.random(), 0.9, 1.3);
+        material.setTextureList([this.m_envTex]);
+        material.initializeByCodeBuf(material.getTextureAt(0) != null);
+
+        let sph = this.m_sphEntity;
+        if (this.m_sphEntity != null) {
+            sph = VoxEntity.createDisplayEntity();
+            sph.setMaterial(material);
+            sph.copyMeshFrom(this.m_sphEntity);
+        } else {
+            //sph.initialize(radius, 20, 20);
+            this.m_sphEntity = VoxEntity.createSphere(radius, 20, 20, material);
+        }
+
+        sph.setPosition(pv);
+        this.m_rscene.addEntity(sph, 1);
+
+        this.m_entities.push(sph);
+        return sph;
+    }
+    private m_time = 0.0;
+    private run(evt: IEventBase = null): void {
+
+        let ls = this.m_entities;
+        let len = this.m_entities.length;
+        let pos = VoxMath.createVec3();
+        for (let i = 0; i < len; ++i) {
+            const factor = Math.sin(i * 0.5 + this.m_time);
+            const et = ls[i];
+            et.getPosition(pos);
+            pos.y = factor * 100.0;
+            let scale = Math.abs(factor * 1.5);
+            if(scale < 0.3) {
+                scale = 0.3;
+            }
+            et.setPosition(pos);
+            et.setScaleXYZ(scale, scale, scale);
+        }
+        this.m_time += 0.05;
+    }
+    private makeMaterial(metallic: number, roughness: number, ao: number): IRenderMaterial {
+        let dis = 700.0;
+        let disZ = 400.0;
+        let posList: IVector3D[] = [
+            VoxMath.createVec3(-dis, dis, disZ),
+            VoxMath.createVec3(dis, dis, disZ),
+            VoxMath.createVec3(-dis, -dis, disZ),
+            VoxMath.createVec3(dis, -dis, disZ)
+        ];
+        let colorSize = 300.0;
+        let colorList: IColor4[] = [
+            VoxMaterial.createColor4().randomRGB(colorSize),
+            VoxMaterial.createColor4().randomRGB(colorSize),
+            VoxMaterial.createColor4().randomRGB(colorSize),
+            VoxMaterial.createColor4().randomRGB(colorSize)
+        ];
+
+        let material = new PBREnvLightingMaterialWrapper();
+        material.setMetallic(metallic);
+        material.setRoughness(roughness);
+        material.setAO(ao);
+
+        for (let i: number = 0; i < 4; ++i) {
+            material.setPosAt(i, posList[i]);
+            material.setColorAt(i, colorList[i]);
+        }
+        material.setAlbedoColor(VoxMaterial.createColor4().randomRGB(1.0));
+        return material.material;
     }
 }
 
 export class TransformTest {
 
     private m_rscene: IRendererScene = null;
-    private m_materialObjs: MaterialObj[] = [];
     constructor() { }
 
     initialize(): void {
@@ -93,67 +150,8 @@ export class TransformTest {
         this.m_rscene.addEventListener(MouseEvent.MOUSE_DOWN, this, this.mouseDown);
     }
 
-    private getTexByUrl(url: string): IRenderTexture {
-
-        let tex = this.m_rscene.textureBlock.createImageTex2D(64, 64, false);
-        let img = new Image();
-        img.onload = (): void => {
-            tex.setDataFromImage(img);
-        };
-        img.src = url;
-        return tex;
-    }
-
-    private createMaterial(r: number, g: number, b: number, texUrl: string): MaterialObj {
-
-        let data = new Float32Array([r, g, b, 1.0]);
-        let material = VoxMaterial.createShaderMaterial("tutorial_shader");
-        material.setFragShaderCode(fragShaderCode);
-        material.setVertShaderCode(vertShaderCode);
-        material.addUniformDataAt("u_color", data);
-        material.setTextureList([this.getTexByUrl( texUrl )]);
-        let obj = new MaterialObj(material, data);
-        this.m_materialObjs.push( obj );
-        return obj;
-    }
-    private entityMouseOver(evt: IMouseEvent): void {
-        console.log("entityMouseDown(), evt.uuid: ", evt.uuid);
-        if(evt.uuid == "cube") {
-            this.m_materialObjs[0].setRGB3f(Math.random(), Math.random(), Math.random());
-        }else if(evt.uuid == "sph") {
-            this.m_materialObjs[1].setRGB3f(Math.random(), Math.random(), Math.random());
-        }
-    }
-    private entityMouseOut(evt: IMouseEvent): void {
-        console.log("entityMouseDown(), evt.uuid: ", evt.uuid);
-        if(evt.uuid == "cube") {
-            this.m_materialObjs[0].setRGB3f(Math.random(), Math.random(), Math.random());
-        }else if(evt.uuid == "sph") {
-            this.m_materialObjs[1].setRGB3f(Math.random(), Math.random(), Math.random());
-        }
-    }
-    private initEntityMouseEvent(entity: IMouseEventEntity, uuid: string): void {
-
-        entity.uuid = uuid;
-        entity.mouseEnabled = true;
-        entity.addEventListener(MouseEvent.MOUSE_OVER, this, this.entityMouseOver);
-        entity.addEventListener(MouseEvent.MOUSE_OUT, this, this.entityMouseOut);
-    }
     private init3DScene(): void {
         
-        let materialObj = this.createMaterial(0.1, 1.0, 0.2, "static/assets/metal.png");
-
-        let cube = VoxEntity.createCube(200, materialObj.material);
-        cube.setXYZ(-300, 200, 0);
-        this.m_rscene.addEntity(cube);
-        this.initEntityMouseEvent(cube, "cube");
-
-        let sphMaterial = this.createMaterial(1.0, 0.1, 0.2, "static/assets/box.jpg");
-
-        let sph = VoxEntity.createSphere(150, 20, 20, sphMaterial.material);
-        sph.setXYZ(300, 200, 0);
-        this.m_rscene.addEntity(sph);
-        this.initEntityMouseEvent(sph, "sph");
     }
     run(): void {
         if (this.m_rscene != null) {
