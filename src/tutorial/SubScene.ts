753a937bbe3af66ef9ec5d97735dee33c69cdc63
vilyLei <vily313@126.com>
2023-03-06 22:13:06 +0800

    fix: remove some redundant codes.

--- src/tutorial/SubScene.ts
+++ src/tutorial/SubScene.ts
@@ -19,15 +19,10 @@ export class SubScene {
     initialize(): void {
 
         new VoxModuleShell().initialize(
-            (): void => { this.initMouseInteract(); },
+            (): void => {},
             (): void => { this.initRenderer(); },
             (): void => { this.init3DScene(); }
         );
-    }
-    private initMouseInteract(): void {
-
-        // const mi = VoxUIInteraction.createMouseInteraction();
-        // mi.initialize(this.m_rscene, 0, true).setAutoRunning(true);
     }
     private initRenderer(): void {
 
@@ -46,7 +41,7 @@ export class SubScene {
         rparam.autoSyncRenderBufferAndWindowSize = false;
         rparam.setCamPosition(1000.0, 1000.0, 1000.0);
         rparam.setCamProject(45, 20.0, 9000.0);
-        // this.m_rscene = VoxRScene.createRendererScene(rparam).setAutoRunning(true);
+        
         this.m_rscene = graph.createScene(rparam);
         this.m_rscene.setClearUint24Color(0x888888);
 
@@ -80,34 +75,6 @@ export class SubScene {
         this.m_subRscene.addEntity(bgPlane);
 
         this.m_subRscene.addEntity( VoxEntity.createAxis3DEntity(300) );
-
-        // let planeMaterial = VoxMaterial.createDefaultMaterial();
-        // planeMaterial.normalEnabled = true;
-        // planeMaterial.setUVScale(4.0, 4.0);
-        // planeMaterial.setRGB3f(0.7, 1.0, 0.3);
-        // planeMaterial.setTextureList([this.getTexByUrl("static/assets/box.jpg")]);
-        // let ground = VoxEntity.createXOZPlane(-350, -350, 700, 700, planeMaterial);
-        // ground.setXYZ(0, -100, 0);
-        // /**
-        //  * 渲染状态设置为没有face剔除，混合模式为实色混合, 深度检测方式: (true, LESS)
-        //  */
-        // ground.setRenderState(RendererState.NONE_CULLFACE_NORMAL_STATE);
-        // this.m_rscene.addEntity(ground);
-
-        // for (let i = 0; i < 20; ++i) {
-        //     planeMaterial = VoxMaterial.createDefaultMaterial();
-        //     planeMaterial.setTextureList([this.getTexByUrl("static/assets/flare_core_02.jpg")]);
-        //     const plane = VoxEntity.createYOZPlane(-50, -50, 100, 100, planeMaterial);
-        //     plane.setXYZ(-200 + i * 20, 0, 0);
-        //     /**
-        //      * 渲染状态设置为没有face剔除，混合模式为ADD模式, 深度检测方式: (false, LESS)
-        //      */
-        //     plane.setRenderState(RendererState.NONE_ADD_BLENDSORT_STATE);
-        //     /**
-        //      * 强制保证这些plane在ground之后绘制，以便正确的呈现混合效果
-        //      */
-        //     this.m_rscene.addEntity(plane, 1);
-        // }
     }
 }
 
